68b27549ebd6aa4325824a5135258b8efb868ef4
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-04 09:27:26 +0100

    Create Biosaiwithddrfourheaders.h

---
+++ Biosaiwithddrfourheaders.h
@@ -0,0 +1,5 @@
+//as we can merge bios ak commands with ddr4 algorithms
+//in this top repository, it would be good to have
+//a common communicate links and to have a merged stuff with the cpuai.
+//but for the cpuai, a good way would be to not use deep learnint because
+//of online ui/ux programs models.
