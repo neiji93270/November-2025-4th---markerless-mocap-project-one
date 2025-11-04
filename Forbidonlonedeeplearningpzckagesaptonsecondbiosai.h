13b22ea183c3794a3bedf0abf97cc197e532749c
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-04 09:57:23 +0100

    Create Forbidonlonedeeplearningpzckagesaptonsecondbiosai.h

---
+++ Forbidonlonedeeplearningpzckagesaptonsecondbiosai.h
@@ -0,0 +1,6 @@
+//we can extract apt responses with generated lines of code
+//if the second bios ai can reverse 15 existing lines of code internally.
+
+//this rough algorithm and behaviour can boot on usb keys.
+//so the usb keys must contain oarc datas. rge cyber power supply
+//systems must take 10^17 years to enable those commands 
